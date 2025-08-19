c8a9493ab6042db47e8e07430d7f77fdc035c955
shabihosama@gmail.com <shab>
2025-08-19 20:24:09 +0530

    Dependenchy Integerations

---
+++ Dependenchy Injection/Database-connection.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+
+# dependenchy functions
+def get_db():
+    db = {'connection':'mock_db_connection'}
+    try:
+        yield db
+        
+    finally:
+        db.close()
+
+
+
+
+
+#. endpoindt
+@app.get('/home')
+def home(db=Depends(get_db)):
+    return {'db_status':db['connection']}
---
+++ middleware/gzip.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from starlette.middleware.gzip import GZipMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    GZipMiddleware,minimum_size=1000
+    
+)
