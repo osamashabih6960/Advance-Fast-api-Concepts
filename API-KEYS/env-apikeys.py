5f26dfa7cb1f415d498a141dd0cb0c05a94ca721
shabihosama@gmail.com <shab>
2025-08-20 12:59:45 +0530

    API_KEYS

--- API-KEYS/env-apikeys.py
+++ API-KEYS/env-apikeys.py
@@ -14,4 +14,10 @@ settings = Settings()
 app = FastAPI()
 
 def get_api_key(api_key: str = Header(...)):
-    if api_key
+    if api_key != settings.api_key:
+        raise HTTPException(status_code=403,detail='Unauthorized')
+    return api_key
+
+@app.get('/get.data')
+def get_data(api_key: str = Depends(get_api_key)):
+    return {'output':'Access granted!'}
